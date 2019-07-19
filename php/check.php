e9bac661b14148a5741c15ed3af5f90c5805958b
Arthur Unity <apploidyakutsk@gmail.com>
2019-07-19 13:56:49 +0900

    integrated with php scripts

--- php/check.php
+++ php/check.php
@@ -4,17 +4,24 @@
 
     $currentTime = date("H:i:s", strtotime('-1 hour'));
     $start = microtime(true);
-    $x = (float) str_replace(",", ".", $_POST['x_value']);
-    $y = (int) $_POST['y_value'];
-    $r = (int) $_POST['r_value'];
+
+    $x = (int) str_replace(",", ".", $_POST['x_value']);
+    $y = (float) $_POST['y_value'];
+    $r = (double) $_POST['r_value'];
+
+    if (!in_array($x, array(-4, -3, -2, -1, 0, 1, 2, 3, 4)) || !is_numeric($y) || $y < -3 || $y > 5 || !in_array($r, array( 1, 1.5, 2, 2.5, 3))) {
+        http_response_code(400);
+        return;
+    }
 
     function check($x, $y, $r){
-        return true;
+        if (($x >= -$r/2 && $x <= 0 && $y <= $r && $y >= 0) || ($y >= (-$x/2 - $r/2) && $y <= 0 && $x <= 0) || (($x*$x + $y*$y) <= $r*$r && $x >= 0 && $y >= 0)){
+            return "<span style='color: green'>True</span>";
+        } else {
+            return "<span style='color: red'>False</span>";
+        }
     }
-//    if (!in_array($y, array(-5, -4, -3, -2, -1, 0, 1, 2, 3)) || !is_numeric($x) || $x < -3 || $x > 5 || !in_array($r, array( -3, -2, -1, 0, 1, 2, 3, 4, 5))) {
-//        http_response_code(400);
-//        return;
-//    }
+
     $res = check ($x, $y, $r);
     $time = microtime(true) - $start;
     $result = array($x, $y, $r, $res, $currentTime, $time);
--- php/table.php
+++ php/table.php
@@ -1,47 +1,22 @@
-<tr>
-    <td>
-        <h2 class="ansPoint">X</h2>
-    </td>
-    <td>
-        <h2 class="ansPoint">Y</h2>
-    </td>
-    <td>
-        <h2 class="ansPoint">R</h2>
-    </td>
-    <td>
-        <h2 class="ansPoint">Result</h2>
-    </td>
-    <td>
-        <h2 class="ansPoint">Time</h2>
-    </td>
-    <td>
-        <h2 class="ansPoint">ScriptTime [us]</h2>
-    </td>
-</tr>
-
-
-<?php
-foreach ($_SESSION['history'] as $value) { ?>
-
-    <tr>
-        <td>
-            <h2 class="ansPoint"><?php echo $value[0] ?></h2>
-        </td>
-        <td>
-            <h2 class="ansPoint"><?php echo $value[1] ?></h2>
-        </td>
-        <td>
-            <h2 class="ansPoint"><?php echo $value[2] ?></h2>
-        </td>
-        <td>
-            <h2 class="ansPoint"><?php echo $value[3] ? "True" : "False" ?></h2>
-        </td>
-        <td>
-            <h2 class="ansPoint"><?php echo $value[4] ?></h2>
-        </td>
-        <td>
-            <h2 class="ansPoint"><?php echo number_format($value[5], 10, ".", "")*1000000 ?></h2>
-        </td>
-    </tr>
-
-<?php }?>
+<div class="table">
+    <div class="table-header">
+        <div class="header__item"><a id="x-table" class="filter__link" href="#">X</a></div>
+        <div class="header__item"><a id="y-table" class="filter__link filter__link--number" href="#">Y</a></div>
+        <div class="header__item"><a id="r-table" class="filter__link filter__link--number" href="#">R</a></div>
+        <div class="header__item"><a id="result-table" class="filter__link filter__link--number" href="#">result</a></div>
+        <div class="header__item"><a id="time-table" class="filter__link filter__link--number" href="#">Time</a></div>
+        <div class="header__item"><a id="cr-time-table" class="filter__link filter__link--number" href="#">Benchmark</a></div>
+    </div>
+    <?php foreach ($_SESSION['history'] as $value) { ?>
+        <div class="table-content">
+            <div class="table-row">
+                <div class="table-data"><?php echo $value[0] ?></div>
+                <div class="table-data"><?php echo $value[1] ?></div>
+                <div class="table-data"><?php echo $value[2] ?></div>
+                <div class="table-data"><?php echo $value[3] ?></div>
+                <div class="table-data"><?php echo $value[4] ?></div>
+                <div class="table-data"><?php echo number_format($value[5], 10, ".", "")*1000000 ?></div>
+            </div>
+        </div>
+    <?php }?>
+</div>
